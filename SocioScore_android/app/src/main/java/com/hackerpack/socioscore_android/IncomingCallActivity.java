7e822c80108e1a86d31c428e484e5a4927daa414
bharathimit <bharathi.mit@gmail.com>
2016-11-05 20:02:03 -0400

    Get SocioScore from REST

--- SocioScore_android/app/src/main/java/com/hackerpack/socioscore_android/IncomingCallActivity.java
+++ SocioScore_android/app/src/main/java/com/hackerpack/socioscore_android/IncomingCallActivity.java
@@ -1,11 +1,15 @@
 package com.hackerpack.socioscore_android;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.WindowManager;
 import android.widget.TextView;
+
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 public class IncomingCallActivity extends Activity {
 
@@ -19,19 +23,50 @@ public class IncomingCallActivity extends Activity {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL);
 
-            setContentView(R.layout.main);
-
-            String number = getIntent().getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-            TextView text = (TextView) findViewById(R.id.text);
-            text.setText("Call From: " + number +
-                    "\n Caller name: "+"Test Name"+
-                    "\n SocioScore: "+ 900
-            );
-        }
-        catch (Exception e) {
+            String phoneNumber = getIntent().getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+            HttpRequestTask httpRequestTask = new HttpRequestTask();
+            httpRequestTask.execute(phoneNumber);
+        } catch (Exception e) {
             Log.d("Exception", e.toString());
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+    }
+
+    private class HttpRequestTask extends AsyncTask<String, Void, Score> {
+        @Override
+        protected Score doInBackground(String... params) {
+            String phoneNumber = params[0];
+            Log.d("HttpRequestTask", phoneNumber);
+            try {
+                final String url = "http://10.126.79.76:3000/phone/"+phoneNumber;
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                Score score = restTemplate.getForObject(url, Score.class);
+                return score;
+            } catch (Exception e) {
+                Log.e("MainActivity", e.getMessage(), e);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Score score) {
+
+            setContentView(R.layout.main);
+
+            TextView text = (TextView) findViewById(R.id.text);
+            text.setText("Call From: " + score.getPhonenumber() +
+                    "\n Caller name: " + score.getName() +
+                    "\n SocioScore: " + score.getScore()
+            );
+
+//            TextView greetingIdText = (TextView) findViewById(R.id.id_value);
+//            TextView greetingContentText = (TextView) findViewById(R.id.content_value);
+//            greetingIdText.setText(score.getId());
+//            greetingContentText.setText(score.getContent());
+        }
+
     }
 }
